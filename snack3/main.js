f0ed3a18d541e4f284e128ef9899c41ca6cfa8b6
marco-carangelo <marcocarangelo2016@gmail.com>
2024-05-23 11:53:31 +0200

    add: animals array

--- snack3/main.js
+++ snack3/main.js
@@ -1,2 +1,45 @@
 'use strict'
 
+//Array contenente gli oggetti relativi ad ogni singolo animale
+const animali = [
+    {
+        nome: 'gatto',
+        famiglia: 'felidi',
+        classe: 'mammiferi',
+    },
+    {
+        nome: 'ecletto',
+        famiglia: 'psittacidi',
+        classe: 'uccelli',
+    },
+    {
+        nome: 'anaconda',
+        famiglia: 'boidae',
+        classe: 'rettili',
+    },
+    {
+        nome: 'panda',
+        famiglia: 'ursidi',
+        classe: 'mammiferi',
+    },
+    {
+        nome: 'corvo',
+        famiglia: 'corvidae',
+        classe: 'uccelli',
+    },
+    {
+        nome: 'allosauro',
+        famiglia: 'allosauridae',
+        classe: 'sauropsida',
+    },
+    {
+        nome: 'delfino',
+        famiglia: 'delphinidae',
+        classe: 'mammiferi',
+    },
+
+];
+
+//Array vuoto che conterrà solo gli oggetti con classe 'mammiferi'
+
+mammiferi = [];
