2b60f88d1d9343a25fce772b825b88ebab6f49e3
marco-carangelo <marcocarangelo2016@gmail.com>
2024-05-23 13:48:51 +0200

    add: array peopleDriveAge with new property

--- snack4/main.js
+++ snack4/main.js
@@ -4,43 +4,77 @@
 
 const people = [
     {
-        nome: 'Giovanni',
-        cognome: 'Rossi',
-        eta: 27,
+        name: 'Giovanni',
+        lastName: 'Rossi',
+        age: 27,
     },
     {
-        nome: 'Eleonora',
-        cognome: 'Bianchi',
-        eta: 15,
+        name: 'Eleonora',
+        lastName: 'Bianchi',
+        age: 15,
     },
     {
-        nome: 'Virginia',
-        cognome: 'Verdi',
-        eta: 20,
+        name: 'Virginia',
+        lastName: 'Verdi',
+        age: 20,
     },
     {
-        nome: 'Federico',
-        cognome: 'Gialli',
-        eta: 55,
+        name: 'Federico',
+        lastName: 'Gialli',
+        age: 55,
     },
     {
-        nome: 'Anna',
-        cognome: 'neri',
-        eta: 25,
+        name: 'Anna',
+        lastName: 'neri',
+        age: 25,
     },
     {
-        nome: 'Viola',
-        cognome: 'Violi',
-        eta: 13,
+        name: 'Viola',
+        lastName: 'Violi',
+        age: 13,
     },
     {
-        nome: 'Andrea',
-        cognome: 'Magenta',
-        eta: 30,
+        name: 'Andrea',
+        lastName: 'Magenta',
+        age: 30,
     },
 
 ]
 
 //Stampa di controllo dell'array appena creato
 
-console.log(people);
+console.log('people', people);
+
+//Creazione secondo array che conterrà il risultato dell'elaborazione
+
+const peopleDriveAge = [];
+
+//Ciclo for per iterare sui vari oggetti dell'array people[] e per popolare l'array peopleDriveAge[]
+
+for (let i = 0; i < people.length; i++) {
+
+    //Copiamo l'oggetto all'indice corrente di people[] in un oggetto temporaneo
+    const currentPerson = people[i];
+
+    //Verifichiamo se la persona ha un'età minore di 18
+    if (currentPerson.age < 18) {
+
+        currentPerson.driveAge = `${currentPerson.name} ${currentPerson.lastName} non è maggiorenne, quindi non può guidare`;
+
+    } else {
+
+        currentPerson.driveAge = `${currentPerson.name} ${currentPerson.lastName} è maggiorenne, quindi può guidare se ha una patente valida`;
+
+
+    }
+
+    //Pushamo l'oggeto temporaneo nell'array peopleDriveAge[]
+
+    peopleDriveAge.push(currentPerson);
+
+
+}
+
+//Stampiamo l'array peopleDriveAge
+
+console.log('peopleDriveAge', peopleDriveAge);
