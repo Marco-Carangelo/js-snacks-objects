07989f72d0fcfabd515694c02a2bcbe8967f31e1
marco-carangelo <marcocarangelo2016@gmail.com>
2024-05-23 12:56:51 +0200

    add: snack 4 array created

--- snack4/main.js
+++ snack4/main.js
@@ -1,2 +1,46 @@
 'use strict'
 
+//Array che contiene gli oggetti relative alle varie persone
+
+const people = [
+    {
+        nome: 'Giovanni',
+        cognome: 'Rossi',
+        eta: 27,
+    },
+    {
+        nome: 'Eleonora',
+        cognome: 'Bianchi',
+        eta: 15,
+    },
+    {
+        nome: 'Virginia',
+        cognome: 'Verdi',
+        eta: 20,
+    },
+    {
+        nome: 'Federico',
+        cognome: 'Gialli',
+        eta: 55,
+    },
+    {
+        nome: 'Anna',
+        cognome: 'neri',
+        eta: 25,
+    },
+    {
+        nome: 'Viola',
+        cognome: 'Violi',
+        eta: 13,
+    },
+    {
+        nome: 'Andrea',
+        cognome: 'Magenta',
+        eta: 30,
+    },
+
+]
+
+//Stampa di controllo dell'array appena creato
+
+console.log(people);
